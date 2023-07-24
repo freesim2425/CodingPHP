011a6a803be189dacab87c6f6adc935ede4bc90c
RayWay003 <freesim2425@gmail.com>
2023-07-24 16:42:12 +0700

    เรียกใช้ไฟล์ด้านนอกมาแสดง

---
+++ menu.php
@@ -0,0 +1,15 @@
+<ul>
+    <li>menu1</li>
+    <li>menu2</li>
+    <li>menu3</li>
+</ul>
+<?php
+function address(){
+    echo "สาขาแม่สะเรียง";
+
+}
+function address2($add){
+    print "ที่อยู่".$add;
+}
+
+?>
---
+++ menuFooter.php
@@ -0,0 +1,9 @@
+<?php
+$company = "Raycompany";
+$year = 2020;
+?>
+<footer>
+    <?php echo $company; ?>
+    <?php echo "&copy" ?>
+    <?php echo $year; ?>
+</footer>
