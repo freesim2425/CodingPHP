d9af1a4672f957cc4c7305f9a884c4b4129a56dd
RayWay003 <freesim2425@gmail.com>
2023-07-26 16:06:00 +0700

    การสร้าง Array แบบ Range

---
+++ arrayrange.php
@@ -0,0 +1,16 @@
+<?php
+// start, stop, step
+$number = range(1,10,2);
+print_r($number);
+// ผลลัพธ์ Array ( [0] => 1 [1] => 3 [2] => 5 [3] => 7 [4] => 9 ) ค่าความต่าง 2 เหมือน 1+2 = 3+2 =5 บวกกันเลื่อยๆเป็นต้น
+
+// $word = range('A','Z',2);
+// print_r($word);
+// ผลลัพธ์ Array ( [0] => A [1] => C [2] => E [3] => G [4] => I [5] => K [6] => M [7] => O [8] => Q [9] => S [10] => U [11] => W [12] => Y )
+
+//$number = range(1,10);
+//print_r($number);
+
+// $word = range('A','Z');
+// print_r($word);
+?>
